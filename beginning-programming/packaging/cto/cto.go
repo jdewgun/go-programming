c2dbe604937d96115a814c92518b88220abad524
Jatan Dewgun <jatan.dewgun@everoad.com>
2020-03-16 14:36:39 +0100

    Additionally FooBar

--- beginning-programming/packaging/cto/cto.go
+++ beginning-programming/packaging/cto/cto.go
@@ -2,5 +2,12 @@ package main
 
 import (
 	"fmt"
-	dev ""
+	dev "github.com/jdewgun/go-programming/beginning-programming/packaging/developer"
 )
+
+func main () {
+	fmt.Println("CTO is now with the Developer")
+	dev.Code()
+	dev.reviewMR()
+	dev.coffeeBreak()
+}
---
+++ beginning-programming/packaging/developer/developer.go
@@ -0,0 +1,15 @@
+package developer
+
+import "fmt"
+
+func Code () {
+	fmt.Println("Developer is now coding.")
+}
+
+func coffeeBreak () {
+	fmt.Println("Developer is now taking a coffee break.")
+}
+
+func reviewMR () {
+	fmt.Println("Developer is reviewing MR \n .... \n MR Done")
+}
